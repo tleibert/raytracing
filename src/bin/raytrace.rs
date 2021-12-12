d03f72cc7e25b30d305aa48df9c5e6de8b4235ab
Trevor Leibert <tleibert1999@gmail.com>
2021-12-11 19:41:52 -0500

    Revise hit sphere function

--- src/bin/raytrace.rs
+++ src/bin/raytrace.rs
@@ -5,14 +5,15 @@ use raytracing::vec::{Color, Point3, Vec3};
 
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - center;
-    let a = r.direction().dot(r.direction());
-    let b = 2.0 * oc.dot(r.direction());
-    let c = oc.dot(oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.direction().length().powi(2);
+    let half_b = oc.dot(r.direction());
+    let c = oc.length().powi(2) - radius * radius;
+    let discriminant = half_b * half_b - a * c;
+
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b - discriminant.sqrt()) / (2.0 * a)
+        (-half_b - discriminant.sqrt()) / a
     }
 }
 
